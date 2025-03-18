07766819fc76f610edc23eadd5d8505870e3f28b
Jonathan Jansson <h23jjans@du.se>
2025-03-18 10:49:17 +0100

    Börjat på server

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+            validate: {
+                len: [4, 200],
+                isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING(50),
+            validate: {
+                len: [3, 50]
+            }
+        },
+        firstName: DataTypes.STRING(50),
+        lastName: DataTypes.STRING(50),
+        description: DataTypes.TEXT,
+        imageUrl: {
+           type: DataTypes.STRING(50),
+           validate: {
+            isUrl: true
+           }
+        } 
+    },  { underscored: true }
+);
+};
